864b311e4562c407488fb7022941c859868d0f1b
Trufafa <illmakeitworkthistime@gmail.com>
2026-02-01 05:53:57 +0000

    Added Crowbars and Doors and Penis

---
+++ Assets/Scripts/Item/CameraBehaviour.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public void Shoot()
+    {
+        
+    }
+}
--- Assets/Scripts/Player/ItemBehaviour.cs
+++ Assets/Scripts/Player/ItemBehaviour.cs
@@ -9,7 +9,7 @@ public class ItemBehaviour : MonoBehaviour
     [SerializeField] private PlayerMovement _playerMovement;
     [SerializeField] private GameObject _firecracker;
     [SerializeField] private GameObject _dogTreat;
-
+    [SerializeField] private float _crowbarRadius = 0.55f;
     private void Update()
     {
         
@@ -24,5 +24,25 @@ public class ItemBehaviour : MonoBehaviour
         GameObject instantiatedItem = Instantiate(selectedItem, transform.position, quaternion.identity);
         ThrowableItem thrownItem = instantiatedItem.GetComponent<ThrowableItem>();
         thrownItem.InitialImpulse(_playerMovement.Direction);
+    }
+    [Button(enabledMode: EButtonEnableMode.Playmode)]
+    public void UseCrowbar()
+    {
+        Collider2D[] targetsObtained = Physics2D.OverlapCircleAll(transform.position, _crowbarRadius);
+        for (int i = 0; i < targetsObtained.Length; i++)
+        {
+            DoorInteract door = targetsObtained[i].GetComponent<DoorInteract>();
+            if (door != null)
+            {
+                door.BreakDownDoor();
+                break;
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.papayaWhip;
+        
+        Gizmos.DrawWireSphere(transform.position, _crowbarRadius);
     }
 }
