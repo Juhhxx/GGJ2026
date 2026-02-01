d7444c2f3e171ade634a2e70799fcfcd5816bb65
Trufafa <illmakeitworkthistime@gmail.com>
2026-02-01 03:21:05 +0000

    Add Sneak

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,17 +1,34 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 12f;
+    [SerializeField] private float _sneakMultiplier = 0.25f;
     [SerializeField] private Rigidbody2D _rb;
+    float moveAmountHorizontal;
+    float moveAmountVertical;
     private void Update()
     {
         Move();
+        Sneak();
+        ApplyMovement();
     }
     private void Move()
     {
-        float moveAmountHorizontal = Input.GetAxisRaw("Horizontal") * _moveSpeed;
-        float moveAmountVertical = Input.GetAxisRaw("Vertical") * _moveSpeed;
+        moveAmountHorizontal = Input.GetAxisRaw("Horizontal") * _moveSpeed;
+        moveAmountVertical = Input.GetAxisRaw("Vertical") * _moveSpeed;
+    }
+    private void Sneak()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            moveAmountHorizontal *= _sneakMultiplier;
+            moveAmountVertical *= _sneakMultiplier;
+        }
+    }
+    private void ApplyMovement()
+    {
         _rb.linearVelocity = new Vector2(moveAmountHorizontal, moveAmountVertical);
     }
 }
